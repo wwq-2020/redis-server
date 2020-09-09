66f852162cb4c3bbf3dfa170dfc538ccb0cd0c42
wwq <2993895632@qq.com>
2020-09-09 10:23:00 +0800

    refactor: simplify structure

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,4 +1,4 @@
-use super::common::{MyResult, MySimpleResult, AM};
+use super::common::{MySimpleResult, AM};
 use super::db::{CmdReq, DB};
 use super::error::{Error, RedisError};
 use memchr::memchr;
@@ -15,7 +15,7 @@ pub struct Client {
     multibulklen: i64,
     bulklen: i64,
     db: AM<DB>,
-    req: CmdReq,
+    req: Option<CmdReq>,
 }
 
 enum ReqType {
@@ -36,7 +36,7 @@ impl Client {
             multibulklen: 0,
             bulklen: -1,
             db: db,
-            req: CmdReq::new(),
+            req: Some(CmdReq::new()),
         }
     }
 }
@@ -44,53 +44,101 @@ impl Client {
 impl Client {
     pub async fn serve(&mut self) -> MySimpleResult {
         loop {
-            let n = self.conn.read(&mut self.read_buf).await?;
-            if n == 0 {
-                return Err(Error::Redis(RedisError::new(format!(
-                    "client_id:{:?} conn closed",
-                    self.id
-                ))));
+            self.serve_one().await?;
+        }
+    }
+
+    async fn serve_one(&mut self) -> MySimpleResult {
+        self.read_data().await?;
+        self.parse_req_type()?;
+        self.parse_req()?;
+        self.handle_req().await?;
+        Ok(())
+    }
+
+    async fn read_data(&mut self) -> MySimpleResult {
+        let n = self.conn.read(&mut self.read_buf).await?;
+        if n == 0 {
+            return Err(Error::Redis(RedisError::new(format!(
+                "client_id:{:?} conn closed",
+                self.id
+            ))));
+        }
+        self.data_pos += n;
+        Ok(())
+    }
+
+    fn parse_req_type(&mut self) -> MySimpleResult {
+        self.req_type = match self.req_type {
+            ReqType::Init => {
+                if self.read_buf[self.read_pos] == '*' as u8 {
+                    ReqType::MULTIBULK
+                } else {
+                    ReqType::INLINE
+                }
             }
-            self.data_pos += n;
-            self.req_type = match self.req_type {
-                ReqType::Init => {
-                    if self.read_buf[self.read_pos] == '*' as u8 {
-                        ReqType::MULTIBULK
-                    } else {
-                        ReqType::INLINE
-                    }
-                }
-                ReqType::INLINE => ReqType::INLINE,
-                ReqType::MULTIBULK => ReqType::MULTIBULK,
-            };
-            let cmd_req = match self.req_type {
-                ReqType::MULTIBULK => self.process_multibulk_buffer()?,
-                ReqType::INLINE => self.process_inline_buffer()?,
-                _ => panic!("nerver reached"),
-            };
-            match cmd_req {
-                Some(mut cmd_req) => {
-                    let cmd_resp = cmd_req.exec(self.db.clone())?;
-                    self.conn.write_all(cmd_resp.as_bytes()).await?;
-                    self.conn.flush().await?;
-                    self.data_pos = self.data_pos - self.read_pos;
-                    self.read_pos = 0;
-                }
-                None => continue,
+            ReqType::INLINE => ReqType::INLINE,
+            ReqType::MULTIBULK => ReqType::MULTIBULK,
+        };
+        Ok(())
+    }
+
+    fn parse_req(&mut self) -> MySimpleResult {
+        match self.req_type {
+            ReqType::MULTIBULK => self.process_multibulk_buffer()?,
+            ReqType::INLINE => self.process_inline_buffer()?,
+            _ => {
+                return Err(Error::Redis(RedisError::new(format!(
+                    "client_id:{:?} unknown req_type",
+                    self.id
+                ))))
+            }
+        };
+        Ok(())
+    }
+
+    async fn handle_req(&mut self) -> MySimpleResult {
+        match &self.req {
+            Some(cmd_req) => {
+                let mut cmd_req = cmd_req.clone();
+                let cmd_resp = cmd_req.exec(self.db.clone())?;
+                self.conn.write_all(cmd_resp.as_bytes()).await?;
+                self.conn.flush().await?;
+                self.reset()?;
+                return Ok(());
+            }
+            None => {
+                return Ok(());
             }
         }
     }
 
-    fn process_inline_buffer(&mut self) -> MyResult<Option<CmdReq>> {
-        Ok(Some(CmdReq::new()))
+    fn reset(&mut self) -> MySimpleResult {
+        self.data_pos = self.data_pos - self.read_pos;
+        self.read_pos = 0;
+        self.req.as_mut().map(|req| {
+            req.reset();
+        });
+        Ok(())
     }
 
-    fn process_multibulk_buffer(&mut self) -> MyResult<Option<CmdReq>> {
+    fn process_inline_buffer(&mut self) -> MySimpleResult {
+        self.req = Some(CmdReq::new());
+        Ok(())
+    }
+
+    fn process_multibulk_buffer(&mut self) -> MySimpleResult {
+        self.process_multibulklen()?;
+        self.process_bulks()?;
+        Ok(())
+    }
+
+    fn process_multibulklen(&mut self) -> MySimpleResult {
         if self.multibulklen == 0 {
             let idx = match memchr('\r' as u8, self.cur_data()) {
                 Some(idx) => idx,
                 _ => {
-                    return Ok(None);
+                    return Ok(());
                 }
             };
 
@@ -102,7 +150,7 @@ impl Client {
             }
 
             if idx == self.read_buf.len() - 1 {
-                return Ok(None);
+                return Ok(());
             }
 
             let mut multibulklen: i64 = 0;
@@ -116,18 +164,23 @@ impl Client {
             }
             self.read_pos = self.read_pos + idx + 2;
             if multibulklen <= 0 {
-                return Ok(None);
+                return Ok(());
             }
             self.multibulklen = multibulklen;
-            self.req.args = Vec::with_capacity(multibulklen as usize);
+            self.req.as_mut().map(|req| {
+                req.args = Vec::with_capacity(multibulklen as usize);
+            });
         }
+        Ok(())
+    }
 
+    fn process_bulks(&mut self) -> MySimpleResult {
         while self.multibulklen != 0 {
             if self.bulklen == -1 {
                 let idx = match memchr('\r' as u8, self.cur_data()) {
                     Some(idx) => idx,
                     _ => {
-                        return Ok(None);
+                        return Ok(());
                     }
                 };
                 if self.peek() != '$' as u8 {
@@ -137,7 +190,7 @@ impl Client {
                     ))));
                 }
                 if idx == self.cur_data().len() - 1 {
-                    return Ok(None);
+                    return Ok(());
                 }
 
                 let mut bulklen: i64 = 0;
@@ -161,41 +214,38 @@ impl Client {
             }
 
             if self.read_buf.len() < (self.bulklen + 2) as usize {
-                return Ok(None);
+                return Ok(());
             }
-
-            if self.req.command.is_empty() {
-                self.req.command = self.slice_len(self.bulklen as usize).to_vec();
-            } else {
-                let mut buf = Vec::with_capacity(self.bulklen as usize);
-                buf.extend_from_slice(self.slice_len(self.bulklen as usize));
-                self.req.args.push(buf);
-            }
+            let buf = self.slice_len(self.bulklen as usize).to_vec();
+            self.req.as_mut().map(|req| {
+                if req.command.is_empty() {
+                    req.command = buf
+                } else {
+                    req.args.push(buf);
+                }
+            });
 
             self.read_pos = self.read_pos + self.bulklen as usize + 2;
 
             self.bulklen = -1;
             self.multibulklen -= 1;
         }
-
-        let req = self.req.clone();
-        self.req.reset();
-        return Ok(Some(req));
+        Ok(())
     }
 
-    fn slice_len(&self, len: usize) -> &[u8] {
+    fn slice_len(&mut self, len: usize) -> &[u8] {
         &self.slice_to(self.read_pos + len)
     }
 
-    fn slice_to(&self, end: usize) -> &[u8] {
+    fn slice_to(&mut self, end: usize) -> &[u8] {
         &self.read_buf[self.read_pos..end]
     }
 
-    fn cur_data(&self) -> &[u8] {
+    fn cur_data(&mut self) -> &[u8] {
         self.slice_to(self.data_pos)
     }
 
-    fn peek(&self) -> u8 {
+    fn peek(&mut self) -> u8 {
         self.read_buf[self.read_pos]
     }
 }
--- src/common/mod.rs
+++ src/common/mod.rs
@@ -2,4 +2,3 @@ use super::error::Error;
 use std::sync::{Arc, Mutex};
 pub type AM<T> = Arc<Mutex<T>>;
 pub type MySimpleResult = Result<(), Error>;
-pub type MyResult<T> = Result<T, Error>;
